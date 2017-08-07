0d40d61394a4efc9cfeaa175ae17d948b4424708
Takashi Kitajima <inc@2inc.org>
2017-08-07 11:18:03 +0900

    Add .travis.yml #1

---
+++ bin/tagging.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+set -e
+
+if [[ "false" != "$TRAVIS_PULL_REQUEST" ]]; then
+  echo "Not deploying pull requests."
+  exit
+fi
+
+if [[ "master" != "$TRAVIS_BRANCH" ]]; then
+  echo "Not on the 'master' branch."
+  #exit
+fi
+
+if [[ "7" != "$TRAVIS_PHP_VERSION" ]]; then
+	echo "deploy only PHP 7"
+	exit
+fi
+
+git clone -b release --quiet https://github.com/earn-pocket-money/earn-pocket-money.git release
+cd release
+ls | xargs rm -rf
+ls -la
+cp -r ../. ./
+rm -rf src gulpfile.js package.json yarn.lock
+ls -la
+
+git add -A
+git status
+git commit -m "[ci skip] release branch update from travis $TRAVIS_COMMIT"
+git push origin release
+
+git tag $(wp theme get earn-pocket-money --field=version)
+git push origin --tags
